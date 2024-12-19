e2786ad5dbd0805fc6c030000eb5ee6cc71de59c
Matezzi777 <matezzi777@gmail.com>
2024-12-19 03:39:51 +0100

    Project refactored (using Pygame instead of Tkinter for Visual Stuffs)

--- board.py
+++
@@ -1,2 +0,0 @@
-def board_init() -> list[str]:
-	return (["rnbqkbnr", "pppppppp", "        ", "        ", "        ", "        ", "PPPPPPPP", "RNBQKBNR"])
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from window import *
-from ui import *
-from board import *
+import pygame # type: ignore
 
-#INITIALISATION
-window: tk.Tk = create_window()
-board: list[str] = board_init()
-board_canvas: tk.Canvas = create_board(window)
-place_pieces(board_canvas, board)
-board_canvas.place(x=50, y=50)
-side_canvas: tk.Canvas = create_side_canvas(window)
-side_canvas.place(x=900, y=50)
+# pygame setup
+pygame.init()
+window = pygame.display
+window.set_caption('ChessAI')
 
+screen = window.set_mode((900, 900))
+clock = pygame.time.Clock()
+running = True
 
-window.mainloop()
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("gray")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
